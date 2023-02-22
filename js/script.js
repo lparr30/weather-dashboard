d7396ffa4a5d17f7969c719bfe7179c5842df7bd
Liza Parr <lizajparr@gmail.com>
2023-02-21 23:46:07 -0500

    local storage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var APIkey = "1c337a9e8fd9a9e0a3340f2849e04fa9";
 var searchBtn = document.getElementById("searchBtn");
 
+var savedCity = localStorage.getItem('city');
+var searchHistory = document.getElementById('history');
+searchHistory.append(savedCity);
+
 function performSearch () {
     var inputVal = document.getElementById("cityToSearch").value.trim();
     weatherSearch(inputVal)
@@ -21,7 +25,34 @@ function weatherSearch(city) {
         document.getElementById("temp").textContent = "Temp: " + data.main.temp + " \u00B0F";
         document.getElementById("wind").textContent = "Wind: " + data.wind.speed + " MPH";
         document.getElementById("humid").textContent = "Humidity: " + data.main.humidity + "%";
+        localStorage.setItem('city',city);
     })
 }
 
 searchBtn.addEventListener("click", performSearch);
+
+function forecastSearch() {
+    var userInput = document.getElementById('cityToSearch').value.trim();
+    futureWeatherSearch(userInput);
+}
+
+function futureWeatherSearch(lat,lon) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+    fetch(queryURL)
+    .then(function(result) {
+        return result.json()
+    })
+    .then(function(data) {
+        var today = dayjs().format('MM/DD/YYYY');
+        // var oneDay = dayjs().add(dayjs.duration({'days':1}));
+        document.getElementById('one-day').textContent = oneDay;
+        // var latitude = data.coord.lat;
+        // var longitude = data.coord.lon;
+    })
+    
+
+    
+}
+
+
+searchBtn.addEventListener('click', forecastSearch);
