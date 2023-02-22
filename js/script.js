a8a0cc47f7691e364319efc5f8ac7d89229762bd
Liza Parr <lizajparr@gmail.com>
2023-02-21 19:09:56 -0500

    basic landing page and fetching data

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-var APIkey = "1c337a9e8fd9a9e0a3340f2849e04fa9"
-var city;
+var APIkey = "1c337a9e8fd9a9e0a3340f2849e04fa9";
+var searchBtn = document.getElementById("searchBtn");
+
+function performSearch () {
+    var inputVal = document.getElementById("cityToSearch").value.trim();
+    weatherSearch(inputVal)
+}
+
+function weatherSearch(city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+    fetch(queryURL)
+    .then(function(result) {
+        return result.json()
+    })
+    .then(function (data) {
+        console.log(data);
+        console.log(data.main.temp);
+        document.getElementById("city-name").textContent = data.name;
+        var today = dayjs().format('MM/DD/YYYY');
+        document.getElementById("current-date").textContent = today
+        document.getElementById("temp").textContent = "Temp: " + data.main.temp + "F";
+    })
+}
+
+searchBtn.addEventListener("click", performSearch);
